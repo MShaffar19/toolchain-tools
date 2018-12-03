891d82b7a7c53b5de936ab34c5f29b503b3e2077
Pierre <pfernique@gmail.com>
2018-12-03 08:57:09 +0100

    Update

--- bin/conda/libtoolchain/build.sh
+++ bin/conda/libtoolchain/build.sh
@@ -23,10 +23,10 @@
 
 set -ve
 
-mkdir -p $PREFIX/etc/conda/activate.d
-cp $RECIPE_DIR/activate.sh $PREFIX/etc/conda/activate.d/libtoolchain_vars.sh
+mkdir -p ${PREFIX}/etc/conda/activate.d
+cp ${RECIPE_DIR}/activate.sh ${PREFIX}/etc/conda/activate.d/libtoolchain_vars.sh
 
-mkdir -p $PREFIX/etc/conda/deactivate.d
-cp $RECIPE_DIR/deactivate.sh $PREFIX/etc/conda/deactivate.d/libtoolchain_vars.sh
+mkdir -p ${PREFIX}/etc/conda/deactivate.d
+cp ${RECIPE_DIR}/deactivate.sh ${PREFIX}/etc/conda/deactivate.d/libtoolchain_vars.sh
 
 set +ve
