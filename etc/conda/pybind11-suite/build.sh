c7e9325f350b03743faebe7fbc3736f486c16691
Pierre Fernique <pfernique@gmail.com>
2018-12-04 19:08:10 +0100

    Update

--- etc/conda/pybind11-suite/build.sh
+++ etc/conda/pybind11-suite/build.sh
@@ -2,23 +2,23 @@ set -ve
 
 ${PYTHON} ${RECIPE_DIR}/filter.py --src=${PREFIX} --dst=filtered.pkl
 
-mkdir build
-cd build
+# mkdir build
+# cd build
 
 
-cmake \
-  -DCMAKE_CXX_COMPILER=${CXX} \
-  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DUSE_PYTHON_INCLUDE_DIR=ON \
-  -DPYTHON_EXECUTABLE=${PREFIX}/bin/python \
-  -DPYBIND11_TEST=OFF \
-  ..
+# cmake \
+#   -DCMAKE_CXX_COMPILER=${CXX} \
+#   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+#   -DUSE_PYTHON_INCLUDE_DIR=ON \
+#   -DPYTHON_EXECUTABLE=${PREFIX}/bin/python \
+#   -DPYBIND11_TEST=OFF \
+#   ..
 
-make -j${CPU_COUNT}
-make install
+# make -j${CPU_COUNT}
+# make install
 
-export PYBIND11_USE_CMAKE=1
-cd ..
+# export PYBIND11_USE_CMAKE=1
+# cd ..
 ${PYTHON} setup.py install --single-version-externally-managed --record record.txt
 
 ${PYTHON} ${RECIPE_DIR}/move.py --src=${PREFIX}/include --dst=${SRC_DIR}/Library/include --filtered=filtered.pkl
