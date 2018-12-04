f6d6ed739b7d64cb3605101012aaabe061e8f3bc
Pierre <pfernique@gmail.com>
2018-12-04 08:06:45 +0100

    Update `pybind11-suite` recipe for windows

--- etc/conda/pybind11-suite/bld.bat
+++ etc/conda/pybind11-suite/bld.bat
@@ -4,7 +4,9 @@ echo ON
 if errorlevel 1 exit 1
 
 mkdir build
+if errorlevel 1 exit 1
 cd build
+if errorlevel 1 exit 1
 
 cmake -G "Visual Studio 14 2015 Win64" ^
       -D CMAKE_INSTALL_PREFIX=%PREFIX% ^
