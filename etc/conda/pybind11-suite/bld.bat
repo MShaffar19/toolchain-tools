61f3c396ed9b99e40932a7ab1e92365ff2db99e9
Pierre Fernique <pfernique@gmail.com>
2018-12-04 19:24:25 +0100

    Update

--- etc/conda/pybind11-suite/bld.bat
+++ etc/conda/pybind11-suite/bld.bat
@@ -3,31 +3,31 @@ echo ON
 %PYTHON% %RECIPE_DIR%\\filter.py --src=%PREFIX%\\Library --dst=filtered.pkl
 if errorlevel 1 exit 1
 
-mkdir build
-if errorlevel 1 exit 1
-cd build
-if errorlevel 1 exit 1
+rem mkdir build
+rem if errorlevel 1 exit 1
+rem cd build
+rem if errorlevel 1 exit 1
 
-set PATH="c:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x64\";%PATH%
-if errorlevel 1 exit 1
+rem set PATH="c:\Program Files (x86)\Windows Kits\10\bin\10.0.16299.0\x64\";%PATH%
+rem if errorlevel 1 exit 1
 
-cmake -G "%CMAKE_GENERATOR%" ^
-      -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
-      -D CMAKE_INSTALL_PREFIX=%PREFIX% ^
-      -D USE_PYTHON_INCLUDE_DIR=ON ^
-      -D PYBIND11_TEST=OFF ^
-      ..
-more CMakeFiles\CMakeOutput.log
-if errorlevel 1 exit 1
+rem cmake -G "%CMAKE_GENERATOR%" ^
+rem       -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
+rem       -D CMAKE_INSTALL_PREFIX=%PREFIX% ^
+rem       -D USE_PYTHON_INCLUDE_DIR=ON ^
+rem       -D PYBIND11_TEST=OFF ^
+rem       ..
+rem more CMakeFiles\CMakeOutput.log
+rem if errorlevel 1 exit 1
 
-nmake
-if errorlevel 1 exit 1
+rem nmake
+rem if errorlevel 1 exit 1
 
-nmake install
-if errorlevel 1 exit 1
+rem nmake install
+rem if errorlevel 1 exit 1
 
-set PYBIND11_USE_CMAKE=1
-cd ..
+rem set PYBIND11_USE_CMAKE=1
+rem cd ..
 %PYTHON% setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
 
