944d663b4880ae479edee6ca6cd33e4a0148400a
Pierre Fernique <pfernique@gmail.com>
2018-12-11 19:55:07 +0100

    Update

--- etc/conda/pybind11-suite/bld.bat
+++ etc/conda/pybind11-suite/bld.bat
@@ -12,7 +12,7 @@ set BUILD_CONFIG=Release
 
 cmake -G "Visual Studio 15 2017 Win64" ^
       -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
-      -DUSE_PYTHON_INCLUDE_DIR=ON ^
+      -DUSE_PYTHON_INCLUDE_DIR=OFF ^
       -DPYBIND11_TEST=OFF ^
       --config Release ^
       ..
